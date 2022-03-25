33bbcd11507886bbcc5814070536aa1eb47f2baa
Gonçalo Neves <61800530+o-neves@users.noreply.github.com>
2022-03-25 22:54:55 +0000

    coiso

--- lib/favs.dart
+++ lib/favs.dart
@@ -1,25 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new Favorites());
+  runApp(
+    const MaterialApp(
+      home: Favorites(),
+    ),
+  );
 }
 
 
-class Favorites extends Directionality {
-  const Favorites() : super(textDirection: TextDirection.rtl) ;
+class Favorites extends StatelessWidget {
+  const Favorites({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    /*return MaterialApp(
-      home: Scaffold(appBar: AppBar(title: Text("Lista de Favoritos"),),
-        body:
-        Container(
-          child:
-          Text("Hello! i am inside a container!",
-            style: TextStyle(fontSize : 20)),
-        ),
-      ),
-    );*/
     return CustomScrollView(
       primary: false,
       slivers: <Widget>[
@@ -28,38 +22,30 @@ class Favorites extends Directionality {
           sliver: SliverGrid.count(
             crossAxisSpacing: 10,
             mainAxisSpacing: 10,
-            crossAxisCount: 2,
+            crossAxisCount: 1,
             children: <Widget>[
+
               Container(
                 padding: const EdgeInsets.all(8),
-                child: const Text("He'd have you all unravel at the"),
+                child: const Text("Teste 1"),
                 color: Colors.green[100],
               ),
               Container(
                 padding: const EdgeInsets.all(8),
-                child: const Text('Heed not the rabble'),
+                child: const Text('Teste 2'),
                 color: Colors.green[200],
               ),
               Container(
                 padding: const EdgeInsets.all(8),
-                child: const Text('Sound of screams but the'),
+                child: const Text('Teste 3'),
                 color: Colors.green[300],
               ),
               Container(
                 padding: const EdgeInsets.all(8),
-                child: const Text('Who scream'),
+                child: const Text('Teste 4'),
                 color: Colors.green[400],
               ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text('Revolution is coming...'),
-                color: Colors.green[500],
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text('Revolution, they...'),
-                color: Colors.green[600],
-              ),
+
             ],
           ),
         ),
@@ -67,77 +53,3 @@ class Favorites extends Directionality {
     );
   }
 }
-
-
-
-
-
-
-
-/*
-class Favorites extends StatelessWidget {
-  const Favorites({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const FavoritesPage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-
-class FavoritesPage extends StatefulWidget {
-  const FavoritesPage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<FavoritesPage> createState() => _FavoritesPageState();
-}
-
-class _FavoritesPageState extends State<FavoritesPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-      // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-*/
-
