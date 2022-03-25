0d876675dd6c3afc20535ce34005eee195bb5a38
Gonçalo Neves <61800530+o-neves@users.noreply.github.com>
2022-03-25 23:03:16 +0000

    Update favs.dart

--- lib/favs.dart
+++ lib/favs.dart
@@ -14,42 +14,44 @@ class Favorites extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      primary: false,
-      slivers: <Widget>[
-        SliverPadding(
-          padding: const EdgeInsets.all(20),
-          sliver: SliverGrid.count(
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            crossAxisCount: 1,
-            children: <Widget>[
+    return Container(
+      color: Colors.red,
+      child: CustomScrollView(
+        primary: false,
+        slivers: <Widget>[
+          SliverPadding(
+            padding: const EdgeInsets.all(20),
+            sliver: SliverGrid.count(
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              crossAxisCount: 1,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: const Text("Teste 1"),
+                  color: Colors.green[100],
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: const Text('Teste 2'),
+                  color: Colors.green[200],
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: const Text('Teste 3'),
+                  color: Colors.green[300],
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: const Text('Teste 4'),
+                  color: Colors.green[400],
+                ),
 
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text("Teste 1"),
-                color: Colors.green[100],
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text('Teste 2'),
-                color: Colors.green[200],
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text('Teste 3'),
-                color: Colors.green[300],
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text('Teste 4'),
-                color: Colors.green[400],
-              ),
-
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
