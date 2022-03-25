338ee6d722744a3629fdd22f677d4b79f9aa5f7a
Gonçalo Neves <61800530+o-neves@users.noreply.github.com>
2022-03-25 21:29:48 +0000

    Create favs.dart

---
+++ lib/favs.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new Favorites());
+}
+
+
+class Favorites extends Directionality {
+  const Favorites() : super(textDirection: TextDirection.rtl) ;
+
+  @override
+  Widget build(BuildContext context) {
+    /*return MaterialApp(
+      home: Scaffold(appBar: AppBar(title: Text("Lista de Favoritos"),),
+        body:
+        Container(
+          child:
+          Text("Hello! i am inside a container!",
+            style: TextStyle(fontSize : 20)),
+        ),
+      ),
+    );*/
+    return CustomScrollView(
+      primary: false,
+      slivers: <Widget>[
+        SliverPadding(
+          padding: const EdgeInsets.all(20),
+          sliver: SliverGrid.count(
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            crossAxisCount: 2,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(8),
+                child: const Text("He'd have you all unravel at the"),
+                color: Colors.green[100],
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                child: const Text('Heed not the rabble'),
+                color: Colors.green[200],
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                child: const Text('Sound of screams but the'),
+                color: Colors.green[300],
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                child: const Text('Who scream'),
+                color: Colors.green[400],
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                child: const Text('Revolution is coming...'),
+                color: Colors.green[500],
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                child: const Text('Revolution, they...'),
+                color: Colors.green[600],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
+
+
+
+
+
+/*
+class Favorites extends StatelessWidget {
+  const Favorites({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const FavoritesPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+
+class FavoritesPage extends StatefulWidget {
+  const FavoritesPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<FavoritesPage> createState() => _FavoritesPageState();
+}
+
+class _FavoritesPageState extends State<FavoritesPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+*/
+
