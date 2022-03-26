eb9c8c8d0ecad04d01afd22b4261bf0d5243512b
Rodrigo1771 <rodrigo.goncalves1771@gmail.com>
2022-03-26 02:37:37 +0000

    recipe page almost done

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -15,29 +15,188 @@ class _RecipeState extends State<Recipe> {
       child: ClipRRect(
           borderRadius: BorderRadius.circular(20),
           child: Container(
-              child: CustomScrollView(
-                  slivers: [
-                    const SliverAppBar(
-                      pinned: true,
-                      expandedHeight: 150.0,
-                      flexibleSpace: FlexibleSpaceBar(
-                        title: Text('Demo'),
-                      ),
-                    ),
-                    SliverList(
-                      delegate: SliverChildListDelegate(
-                        [ Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Image.asset("assets/testphoto1.jpg")
+              color: Colors.brown.shade300,
+              child: Material(
+                type: MaterialType.transparency,
+                child: CustomScrollView(
+                    slivers: [
+                      const SliverAppBar(
+                        backgroundColor: Colors.brown,
+                        pinned: true,
+                        expandedHeight: 80.0,
+                        flexibleSpace: FlexibleSpaceBar(
+                          centerTitle: false,
+                          titlePadding:EdgeInsetsDirectional.only(start: 22, bottom: 16) ,
+                          title: Text('Bacalhau com Natas'),
                         ),
-                          Text("Cachorro"),
-
-                        ]
                       ),
-                    )
-                  ]
+                      SliverList(
+                        delegate: SliverChildListDelegate([
+                          Image.asset("images/testphoto1.jpg"),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: const [
+                                      BuildRatingStars(),
+                                      BuildDifficulty(),
+                                    ]
+                                ),
+                                const BuildPriceAndTime(),
+                              ],
+                            ),
+                          ),
+                          const BuildNotes(),
+                          const BuildDescription(),
+                        ]
+                        ),
+                      )
+                    ]
+                ),
               )
           )
+      ),
+    );
+  }
+}
+
+class BuildPriceAndTime extends StatelessWidget {
+  const BuildPriceAndTime({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        const Material(
+          type: MaterialType.transparency,
+          child: Text('39,99€', style: TextStyle(fontSize: 30.0, color: Colors.white)),
+        ),
+        Row(
+          children: const [
+            Icon( Icons.timer,
+              color: Colors.white,
+              size: 18,
+            ),
+            Material(
+              type: MaterialType.transparency,
+              child: Text('3m', style: TextStyle(fontSize: 20.0, color: Colors.white)),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class BuildRatingStars extends StatelessWidget {
+  const BuildRatingStars({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const [
+        Icon(
+          Icons.star,
+          color: Colors.white,
+          size: 25,
+        ),
+        Icon(
+          Icons.star,
+          color: Colors.white,
+          size: 25,
+        ),
+        Icon(
+          Icons.star,
+          color: Colors.white,
+          size: 25,
+        ),
+        Icon(
+          Icons.star,
+          color: Colors.white,
+          size: 25,
+        ),
+        Icon(
+          Icons.star,
+          color: Colors.white,
+          size: 25,
+        ),
+        Material(
+          type: MaterialType.transparency,
+          child: Text('  20 avaliações', style: TextStyle(fontSize: 20.0, color: Colors.white)), ),
+      ],
+    );
+  }
+}
+class BuildDifficulty extends StatelessWidget {
+  const BuildDifficulty({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: const [
+        Icon(
+          Icons.fastfood,
+          color: Colors.white,
+          size: 25,
+        ),
+        Icon(
+          Icons.fastfood,
+          color: Colors.white,
+          size: 25,
+        ),
+        Icon(
+          Icons.fastfood,
+          color: Colors.white,
+          size: 25,
+        ),
+        Material(
+          type: MaterialType.transparency,
+          child: Text('  Difícil', style: TextStyle(fontSize: 20.0, color: Colors.white)), ),
+      ],
+    );
+  }
+}
+
+class BuildNotes extends StatelessWidget {
+  const BuildNotes({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Material(
+        type: MaterialType.transparency,
+        child: Text('iaudsfhiupasdfh\nuahnsifdn\niqewhrquir\nquwirhuiwefndioshfnweo\n', style: TextStyle(fontSize: 20.0, color: Colors.white)),
+      ),
+    );
+  }
+}
+
+class BuildDescription extends StatelessWidget {
+  const BuildDescription({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Material(
+        type: MaterialType.transparency,
+        child: Text('iaudsfhiupasdfh\nuahnsifdn\niqewhrquir\nquwirhuiwefndioshfnweo\n', style: TextStyle(fontSize: 20.0, color: Colors.white)),
       ),
     );
   }
