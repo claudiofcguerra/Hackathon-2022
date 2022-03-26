5cf76147ef43760dbcaf824fa7c4397bb668dd9a
Gonçalo Neves <61800530+o-neves@users.noreply.github.com>
2022-03-26 07:56:54 +0000

    Update favs.dart

--- lib/favs.dart
+++ lib/favs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hackathon_2022/recipe.dart';
 import 'assets/constants.dart' as constants;
 
 
@@ -21,87 +22,198 @@ class Favorites extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: ClipRRect(
           borderRadius: BorderRadius.circular(20),
-          child: Container(
-              child: CustomScrollView(
-                  slivers: [
-                    const SliverAppBar(
-                      pinned: true,
-                      backgroundColor: constants.secondaryColor,
-                      expandedHeight: 80.0,
-                      flexibleSpace: FlexibleSpaceBar(
-                        title: Text('Lista de Favoritos'),
-                      ),
-                    ),
 
-                    SliverFixedExtentList(
+        child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Recipe()),
+                    );
+                  },
+            child: Container(
+
+                child: CustomScrollView(
+                    slivers: [
+                      const SliverAppBar(
+                        pinned: true,
+                        backgroundColor: constants.secondaryColor,
+                        expandedHeight: 80.0,
+                        flexibleSpace: FlexibleSpaceBar(
+                          title: Text('Lista de Favoritos'),
+                        ),
+                      ),
+
+                      SliverFixedExtentList(
                         itemExtent: 170.0,
                         delegate: SliverChildBuilderDelegate(
                                 (BuildContext context, int count) => Padding(
-                                  padding: const EdgeInsets.only(top: 10),
-                                    child: ClipRRect (
-                                        borderRadius: BorderRadius.circular(10),
-                                        child: Container (
-                                            padding: const EdgeInsetsDirectional.only(top:0),
-                                            alignment: Alignment.center,
-                                            color: constants.backgroundColor,
-                                            child: Row(
-                                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                              children: <Widget>[
-                                                Container(
-                                                  //fit: BoxFit.contain, // otherwise the logo will be tiny
-                                                  child: Image.asset(
-                                                      "images/testFood1.jpeg",
-                                                    fit: BoxFit.fill,
-                                                    alignment: Alignment.centerLeft,
-                                                  ),
-                                                  //alignment: Alignment.centerLeft,
-                                                ),
+                                padding: const EdgeInsets.only(top: 10),
+                                child: ClipRRect (
+                                    borderRadius: BorderRadius.circular(10),
+                                    child: Container (
+                                        padding: const EdgeInsetsDirectional.only(top:0),
+                                        alignment: Alignment.center,
+                                        color: constants.backgroundColor,
+                                        child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          children: <Widget>[
+                                            Container(
+                                              //fit: BoxFit.contain, // otherwise the logo will be tiny
+                                              child: Image.asset(
+                                                "images/testFood1.jpeg",
+                                                fit: BoxFit.fill,
+                                                alignment: Alignment.centerLeft,
+                                              ),
+                                              //alignment: Alignment.centerLeft,
+                                            ),
 
 
-                                                Expanded(
+                                            Expanded(
                                                 child: Container(
                                                     height: 1000.0,
                                                     width: 250.0,
                                                     padding: const EdgeInsetsDirectional.only(end: 2),
                                                     alignment: Alignment.center,
                                                     child: Column(
-                                                        mainAxisAlignment: MainAxisAlignment.center,
-                                                        children: const [
-                                                          Material(
+                                                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                                        children:  [
+                                                          const Material(
                                                               type: MaterialType.transparency,
                                                               child: Text('Sopa de Grelos', textAlign: TextAlign.center,
-                                                                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold))
+                                                                  style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold))
                                                           ),
-                                                          Material(
-                                                              type: MaterialType.transparency,
-                                                              child: Text('Preço: ', textAlign: TextAlign.center,
-                                                                  style: TextStyle(fontSize: 20))
+
+                                                          Row(
+                                                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                                              children: [
+                                                                const Material(
+                                                                    type: MaterialType.transparency,
+                                                                    child: Text('90€', textAlign: TextAlign.center,
+                                                                        style: TextStyle(fontSize: 20))
+                                                                ),
+
+                                                                /*const SizedBox(
+                                                                width: 5,
+                                                              ),*/
+
+                                                                Row(
+                                                                  mainAxisAlignment: MainAxisAlignment.center,
+                                                                  children: const [
+                                                                    Icon(
+                                                                      Icons.star,
+                                                                      color: Colors.black,
+                                                                      size: 15,
+                                                                    ),
+                                                                    Icon(
+                                                                      Icons.star,
+                                                                      color: Colors.black,
+                                                                      size: 15,
+                                                                    ),
+                                                                    Icon(
+                                                                      Icons.star,
+                                                                      color: Colors.black,
+                                                                      size: 15,
+                                                                    ),
+                                                                    Icon(
+                                                                      Icons.star,
+                                                                      color: Colors.black,
+                                                                      size: 15,
+                                                                    ),
+                                                                    Icon(
+                                                                      Icons.star,
+                                                                      color: Colors.black,
+                                                                      size: 15,
+                                                                    ),
+                                                                  ],
+                                                                ),
+
+                                                              ]
+
                                                           ),
-                                                          Material(
-                                                              type: MaterialType.transparency,
-                                                              child: Text('Tempo: ', textAlign: TextAlign.center,
-                                                                  style: TextStyle(fontSize: 20))
-                                                          ),
-                                                          Material(
-                                                              type: MaterialType.transparency,
-                                                              child: Text('Dificuldade: ', textAlign: TextAlign.center,
-                                                                  style: TextStyle(fontSize: 20))
+
+                                                          Row(
+                                                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                                              children: [
+                                                                Material(
+                                                                  type: MaterialType.transparency,
+                                                                  child: Row(
+                                                                    mainAxisAlignment: MainAxisAlignment.center,
+                                                                    children: const [
+                                                                      Icon(
+                                                                        Icons.timer,
+                                                                        color: Colors.black,
+                                                                        size: 18,
+                                                                      ),
+                                                                      Text(
+                                                                        "3m",
+                                                                        style: TextStyle(
+                                                                          fontSize: 18,
+                                                                          color: Colors.black,
+                                                                          fontWeight: FontWeight.bold,
+                                                                        ),
+                                                                      ),
+                                                                    ],
+                                                                  ),
+                                                                ),
+
+                                                                /*const SizedBox(
+                                                                width: 5,
+                                                              ),*/
+
+                                                                Row(
+                                                                    mainAxisAlignment: MainAxisAlignment.center,
+                                                                    /*children: <Widget>[];
+                                                              for(var i = 0; i < 3; i++) {
+                                                                children.add(
+                                                                  Icon(
+                                                                    Icons.fastfood,
+                                                                    color: Colors.black,
+                                                                    size: 20,
+                                                                  )
+                                                                );
+                                                              }*/
+
+                                                                    children: const [
+                                                                      Icon(
+                                                                        Icons.fastfood,
+                                                                        color: Colors.black,
+                                                                        size: 20,
+                                                                      ),
+                                                                      Icon(
+                                                                        Icons.fastfood,
+                                                                        color: Colors.black,
+                                                                        size: 20,
+                                                                      ),
+                                                                      Icon(
+                                                                        Icons.fastfood,
+                                                                        color: Colors.black,
+                                                                        size: 20,
+                                                                      )
+                                                                    ]
+                                                                ),
+                                                              ]
                                                           )
+
+
                                                         ]
                                                     )
                                                 )
-                                                )
-                                              ],
                                             )
+                                          ],
                                         )
                                     )
-                                ),
-                          childCount: 10
+                                )
+                            ),
+                            childCount: 10
                         ),
-                    ),
-                  ]
-              )
-          )
+                      ),
+                    ]
+                )
+            ),
+
+        )
+
+
       ),
     );
 
