5ab081483ff569399ac22e98647739aaa2a320bd
cfcguerra <cfilipecolaresguerra@gmail.com>
2022-03-26 21:01:03 +0000

    Added login

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hackathon_2022/assets/constants.dart' as constants;
+import 'package:hackathon_2022/swipe.dart';
 import 'package:material_color_utilities/material_color_utilities.dart';
 
 class LoginPage extends StatefulWidget {
@@ -12,179 +15,229 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    usernameController.dispose();
+    passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Stack(
         children: [
-           Container(
-              width: double.infinity,
-              constraints: const BoxConstraints.expand(),
-              decoration: const BoxDecoration(
+          Container(
+            width: double.infinity,
+            constraints: const BoxConstraints.expand(),
+            decoration: const BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage("images/testFood1.jpeg"), fit: BoxFit.cover)),
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  top: 200,
-                  bottom: 200,
-                  left: 75,
-                  right: 75
-                ),
-                child: Container(
-                  decoration: BoxDecoration(
+                    image: AssetImage("images/testFood1.jpeg"),
+                    fit: BoxFit.cover)),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 200, bottom: 200, left: 75, right: 75),
+              child: Container(
+                decoration: BoxDecoration(
                     borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                      bottomLeft: Radius.circular(10),
-                      bottomRight: Radius.circular(10)
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                        bottomLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10)),
+                    color: Colors.brown[300]?.withOpacity(0.95)),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(left: 8.0, right: 8.0, top: 15),
+                      child: TextFormField(
+                        controller: usernameController,
+                        style: const TextStyle(color: Colors.white70),
+                        decoration: InputDecoration(
+                          focusedBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.transparent, width: 0.0),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30.0)),
+                          ),
+                          filled: true,
+                          fillColor: Colors.brown[200],
+                          enabledBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.transparent, width: 0.0),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30.0)),
+                          ),
+                          prefixIcon: const Icon(
+                            Icons.perm_identity,
+                            color: Colors.white70,
+                          ),
+                          contentPadding: const EdgeInsets.all(5),
+                          //hintText: 'username',
+                          hintText: 'Username',
+                          hintStyle: GoogleFonts.raleway(
+                              textStyle: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.white70,
+                          )),
+                        ),
+                      ),
                     ),
-                    color: Colors.brown[300]?.withOpacity(0.95)
-                  ),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 15),
-                        child: TextFormField(
-                          style: const TextStyle(
-                            color: Colors.white70
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(left: 8.0, right: 8.0, top: 15),
+                      child: TextFormField(
+                        controller: passwordController,
+                        obscureText: true,
+                        style: const TextStyle(color: Colors.white70),
+                        decoration: InputDecoration(
+                          focusedBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.transparent, width: 0.0),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30.0)),
                           ),
-                          decoration: InputDecoration(
-                              focusedBorder: const OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.transparent, width: 0.0),
-                                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                              ),
-                              filled: true,
-                              fillColor: Colors.brown[200],
-                              enabledBorder: const OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.transparent, width: 0.0),
-                                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                              ),
-                            prefixIcon: const Icon(
-                                Icons.perm_identity,
-                              color: Colors.white70,
-                            ),
-                            contentPadding: const EdgeInsets.all(5),
-                            //hintText: 'username',
-                            hintText: 'Username',
-                            hintStyle: GoogleFonts.raleway(
-                                textStyle: const TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.white70,
-                                )
-                            ),
+                          filled: true,
+                          fillColor: Colors.brown[200],
+                          enabledBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.transparent, width: 0.0),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30.0)),
                           ),
-
+                          prefixIcon: const Icon(
+                            Icons.lock,
+                            color: Colors.white70,
+                          ),
+                          contentPadding: const EdgeInsets.all(5),
+                          hintText: 'Password',
+                          hintStyle: GoogleFonts.raleway(
+                              textStyle: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.white70,
+                          )),
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 15),
-                        child: TextFormField(
-                          obscureText: true,
-                          style: const TextStyle(
-                              color: Colors.white70
-                          ),
-                          decoration: InputDecoration(
-                              focusedBorder: const OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.transparent, width: 0.0),
-                                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                              ),
-                              filled: true,
-                              fillColor: Colors.brown[200],
-                              enabledBorder: const OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.transparent, width: 0.0),
-                                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                              ),
-                              prefixIcon: const Icon(
-                                Icons.lock,
-                                color: Colors.white70,
-                              ),
-                              contentPadding: const EdgeInsets.all(5),
-                              hintText: 'Password',
-                              hintStyle: GoogleFonts.raleway(
-                                  textStyle: const TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.white70,
-                                  )
-                              ),
-                          ),
-
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                        child: Row(
-                          children: [
-                            Checkbox(
-                                value: false,
-                                onChanged: (bool) {}
-                            ),
-                            Text(
-                              'Remember me',
-                              style: GoogleFonts.raleway(
-                                  textStyle: const TextStyle(
-                                    fontSize: 15,
-                                    color: Colors.white70,
-                                  )
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.brown[200],
-                            fixedSize: const Size(75, 35),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30.0),
-                            ),
-                            ),
-                          onPressed: () {  },
-
-                          child: Text(
-                            'Login',
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                      child: Row(
+                        children: [
+                          Checkbox(value: false, onChanged: (bool) {}),
+                          Text(
+                            'Remember me',
                             style: GoogleFonts.raleway(
                                 textStyle: const TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.white70,
-                                )
-                            ),
+                              fontSize: 15,
+                              color: Colors.white70,
+                            )),
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.brown[200],
+                          fixedSize: const Size(75, 35),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
                           ),
-
                         ),
-                      )
-                    ],
-                  ),
+                        onPressed: () {
+                          _login();
+                        },
+                        child: Text(
+                          'Login',
+                          style: GoogleFonts.raleway(
+                              textStyle: const TextStyle(
+                            fontSize: 15,
+                            color: Colors.white70,
+                          )),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.brown[200],
+                          fixedSize: const Size(75, 35),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                        ),
+                        onPressed: () {
+                          _signup();
+                        },
+                        child: Text(
+                          'Sign Up',
+                          style: GoogleFonts.raleway(
+                              textStyle: const TextStyle(
+                            fontSize: 15,
+                            color: Colors.white70,
+                          )),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-           )
+            ),
+          )
         ],
       ),
     );
-    /*return Container(
-      width: double.infinity,
-      constraints: const BoxConstraints.expand(),
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("images/testFood1.jpeg"), fit: BoxFit.cover)),
-      child: SafeArea(
+  }
 
-      child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Container(
-            alignment: Alignment.center,
-            child: Column(
-                children: [
-                    Container(
-                      color: Colors.brown[300],
-                    )
-                    ],
-                ),
-          ),
+  Future<void> _login() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+              email: usernameController.value.text,
+              password: passwordController.value.text);
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const SwipePage(),
         ),
-      )
-    );*/
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
+  Future<void> _signup() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: usernameController.value.text,
+              password: passwordController.value.text);
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const SwipePage(),
+        ),
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hackathon_2022/login.dart';
 import 'package:hackathon_2022/submitRecipe.dart';
 import 'package:hackathon_2022/swipe.dart';
 import 'assets/constants.dart' as constants;
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+
   constants.openRecipeDB().then(
     (_) {
       constants.openUserDB().then(
         (_) {
           runApp(
             const MaterialApp(
-              home: SwipePage(),
+              home: LoginPage(),
             ),
           );
         },
